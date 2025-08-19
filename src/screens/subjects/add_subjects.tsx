4924de8705c5f808d1bd502b6482e967145deeb1
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-08-19 09:35:35 +0200

    Firebase firestore > Adding subject to firestore

--- src/screens/subjects/add_subjects.tsx
+++ src/screens/subjects/add_subjects.tsx
@@ -1,11 +1,38 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ActivityIndicator, Alert, TouchableOpacity } from "react-native";
+import firestore from '@react-native-firebase/firestore';
+import { useState } from "react";
 
-export default function AddSubjectScreen () {
-    return  (
+export default function AddSubjectScreen() {
+    const [subjectData, setSubjectData] = useState({
+        title: "",
+        description: "",
+        createdAt: ""
+    });
+    
+    const handleSave = async () => {
+        try {
+            await firestore().collection('subjects').add({
+                title: 'My First Item',
+                description: 'Hello Firestore!',
+                createdAt: firestore.FieldValue.serverTimestamp(),
+            })
+            Alert.alert("Subject added!");
+        } catch (error) {
+            console.error("Error adding subject:", error);
+        }
+    };
+
+
+    return (
         <View style={styles.body}>
             <Text>
-                Add subject screen 
+                Add subject screen
             </Text>
+            <TouchableOpacity
+                onPress={() => handleSave()}
+            >
+                <Text style={styles.text}>Add</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -15,5 +42,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        marginTop: 20
     }
 })
